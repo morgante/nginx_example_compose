129d5450babd9414d593482cebde0faaf6ac2df2
Morgante Pell <morgante.pell@morgante.net>
2017-03-09 16:21:30 -0500

    Add Docker test script

---
+++ test_docker.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+tries=15
+ret=1
+while [ $ret -ne 0 ] && [ $tries -ne 0 ]
+do
+    sleep 3
+
+    curl -f localhost:8333 > /dev/null 2>&1
+    ret=$?
+
+    let "tries -= 1"
+done
+
+echo $ret
+
+if [ $ret -eq 0 ]; then
+    echo "SUCCESS"
+else
+    echo "FAILURE"
+fi
+
+exit $ret
